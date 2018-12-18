44f0aca7c081ff00f2ce195d3df1462bc4e3fef5
Viktor <viktor.een2@gmail.com>
2018-12-18 14:21:36 +0100

    Rolli'n

--- Yatzy/Yatzy/Yatzy.py
+++ Yatzy/Yatzy/Yatzy.py
@@ -1,4 +1,29 @@
+import random
+min = 1
+max = 6
+
+scoresheet = ["ones", "twos", "threes", "fours", "fives", "sixes", "pair", "twopair", "threeOfaKind", "fourOfaKind", "Yatzy"]
+dices = [0,0,0,0,0]
+scoreOne = 0
+scoreTwo = 0
+rollDice = True
+
 def main():
-    print("Hello World")
+    print("")
+    roll()
+
+
+def roll():
+        for i in range(len(dices)):
+            dices[i] = random.randint(min, max)
+        for j in range(len(dices)):
+            print(dices[j])
+        
+
+def winner():
+    print("Not defined")
+
+def sum():
+    print("Not defined")
 
 if __name__ == "__main__": main()
