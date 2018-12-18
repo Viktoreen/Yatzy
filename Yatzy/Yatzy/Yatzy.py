0b99c3cb1fe81689231fbf3c8f1b3be562444aac
Viktor <viktor.een2@gmail.com>
2018-12-18 15:36:03 +0100

    Whiiiaaaa

--- Yatzy/Yatzy/Yatzy.py
+++ Yatzy/Yatzy/Yatzy.py
@@ -1,29 +1,51 @@
 import random
 min = 1
 max = 6
-
 scoresheet = ["ones", "twos", "threes", "fours", "fives", "sixes", "pair", "twopair", "threeOfaKind", "fourOfaKind", "Yatzy"]
 dices = [0,0,0,0,0]
-scoreOne = 0
-scoreTwo = 0
-rollDice = True
 
 def main():
-    print("")
-    roll()
+    print("Välkommen till Yatzy!")
+    playerOne = input("Spelare 1: ")
+    playerTwo = input("Spelare 2: ")
+    scoreOne = 0
+    scoreTwo = 0
+    turns = 0
+    while(turns < len(scoresheet)):
+        print("Nu kastar ", playerOne)
+        roll()
+        scoreOne = sum(turns, scoreOne)
+        print(playerOne, "Summa: ", scoreOne)
+        input()
+        print("Nu kastar " + playerTwo)
+        roll()
+        scoreTwo = sum(turns, scoreTwo)
+        print(playerTwo, "Summa: ", scoreTwo)
+        input()
+        turns = turns + 1
 
+    winner(scoreOne, scoreTwo)
 
 def roll():
+    for i in range(len(dices)):
+        dices[i] = random.randint(min, max)
+    for j in range(len(dices)):
+        print(dices[j])
+
+
+def winner(score1, score2):
+    if(score1 > score2):
+        print("Vinnare är " + playerOne)
+    elif(score1 < score2):
+        print("Vinnare är " + playerTwo)
+    else:
+        print("Det blev oavgjort!")
+
+def sum(turn, score):
+    if(turn <= 5):
         for i in range(len(dices)):
-            dices[i] = random.randint(min, max)
-        for j in range(len(dices)):
-            print(dices[j])
-        
-
-def winner():
-    print("Not defined")
-
-def sum():
-    print("Not defined")
+            if(dices[i] == turn + 1):
+                score = score + dices[i]
+    return score
 
 if __name__ == "__main__": main()
